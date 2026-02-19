d82123c750efd57407cc2d196fe229f2d5e83b29
Aleksander Pivovarov <sachapivorwar@gmail.com>
2026-02-19 08:49:45 +0200

    flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -30,8 +30,8 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
-        Category, 
-        slug=category_slug, 
+        Category,
+        slug=category_slug,
         is_published=True
     )
     post_list = get_published_posts().filter(category=category)
